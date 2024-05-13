e5d52d067c1a412138446984c0e8266e4a0cd0d1
santiagobah <privasin@icloud.com>
2024-05-12 22:15:52 -0600

    Inicio de Productos

--- Productos.cpp
+++ Productos.cpp
@@ -1,143 +1,118 @@
 #include "Productos.h"
 
-Productos::Productos(int id, string UPC, string name, int id_p, float price, float cost, bool has_iva, int stock){
-    _id = id;
-    _UPC = UPC;
-    _name = name;
-    _id_presentation = id_p;
-    _price = price;
-    _cost = cost;
-    _has_iva = has_iva;
-    _stock = stock;
+Productos::Productos(){
+    
 }
 
 Productos::~Productos(){
 }
-
-void Productos::set_id(int id_s){
-    _id = id_s;
-}
-void Productos::set_UPC(string UPC){
-    _UPC = UPC;
-}
-void Productos::set_name(string name){
-    _name = name;
-}
-void Productos::set_id_p(int id_p){
-    _id = id_p;
-}
-void Productos::set_price(float price){
-    _price = price;
-}
-void Productos::set_cost(float cost){
-    _cost = cost;
-}
-void Productos::set_h_iva(bool has_iva){
-    _has_iva = has_iva;
-}
-void Productos::set_stock(int stock){
-    _stock = stock;
-}
-
-int Productos::get_id(){
-    return _id;
-}
-string Productos::get_UPC(){
-    return _UPC;
-}
-string Productos::get_name(){
-    return _name;
-}
-int Productos::get_id_p(){
-    return _id_presentation;
-}
-float Productos::get_price(){
-    return _price;
-}
-float Productos::get_cost(){
-    return _cost;
-}
-int Productos::get_iva(){
-    return _has_iva;
-}
-int Productos::get_stock(){
-    return _stock;
-}
+product NuevoProducto; //Instancia de la estructura product
 void Productos::Menu_Principal(){
     cout << "\nBienvenido al menú de Productos" << endl;
     int opc_men_pod = 0;
-    cout <<
-    "1. Add Products" <<
-    "\n2. Edit Products" <<
-    "\n3. See Products" << //SEE?
-    "\n4. Delete Products" <<
-    "\nOpción elegida: ";
-    cin >> opc_men_pod;
-    switch(opc_men_pod){
-        case 1:
-        {
-            Agregar_Productos();
-            break;
+    do {
+        cout <<
+        "1. Add Products" <<
+        "\n2. Edit Products" <<
+        "\n3. See Products" << //SEE?
+        "\n4. Delete Products" <<
+        "\n5. Return" <<
+        "\nOpción elegida: ";
+        cin >> opc_men_pod;
+        switch(opc_men_pod){
+            case 1:
+            {
+                Agregar_Productos();
+                break;
+            }
+            case 2:
+            {
+                Editar_Productos();
+                break;
+            }
+            case 3:
+            {
+                Ver_Productos();
+                break;
+            }
+            case 4:
+            {
+                Eliminar_Productos();
+                break;
+            }
+            case 5:
+            {
+                break;
+            }
+            default:
+            {
+                cout << "Seleccione una opción válida" << endl;
+                Menu_Principal();
+                break;
+            }
         }
-        case 2:
-        {
-            Editar_Productos();
-            break;
-        }
-        case 3:
-        {
-            Ver_Productos();
-            break;
-        }
-        case 4:
-        {
-            Eliminar_Productos();
-            break;
-        }
-        default:
-        {
-            cout << "Seleccione una opción válida" << endl;
-            Menu_Principal();
-            break;
-        }
-    }
+
+        
+    } while (opc_men_pod != 5);
 }
 void Productos::Agregar_Productos(){
+    //Falta crear encabezados
+    ofstream archivo_p("Productos.csv", ios::app);
     cout << "Let´s start by scanning the product barcode: " << endl;
-    //cin >> escanear con pistola;
+    cin >> NuevoProducto.UPC;
+    archivo_p << endl; //Para asegurarnos de crear una nueva fila
+    archivo_p << NuevoProducto.UPC << ",";
     cout << "Type the product name: ";
-    cin >> _name;
-    cout << "Type the ID for this presentation of " << _name << ": ";
-    cin >> _id_presentation;
-    cout << "Type the price of " << _name << ": ";
-    cin >> _price;
-    cout << "Type the cost of " << _name << ": ";
-    cin >> _cost;
+    cin >> NuevoProducto.name;
+    archivo_p << NuevoProducto.name << ",";
+    cout << "Type the ID for this presentation of " << NuevoProducto.name << ": ";
+    cin >> NuevoProducto.id_presentation;
+    archivo_p << NuevoProducto.id_presentation << ",";
+    cout << "Type the price of " << NuevoProducto.name << ": ";
+    cin >> NuevoProducto.price;
+    archivo_p << NuevoProducto.price << ",";
+    cout << "Type the cost of " << NuevoProducto.name << ": ";
+    cin >> NuevoProducto.cost;
+    archivo_p << NuevoProducto.cost << ",";
     Decision_IVA();
-    cout << "Type the stock of " << _name << ": ";
+    cout << "Type the stock of " << NuevoProducto.name << ": ";
+    cin >> NuevoProducto.stock;
+    archivo_p << NuevoProducto.stock;
+    _productos.push_back(NuevoProducto);
+    archivo_p.close();
 }
 void Productos::Eliminar_Productos(){
     cout << "Type or scan the product barcode: " << endl;
-    //cin >> pistola
+    cin >> NuevoProducto.UPC;
+    for (int i = 0; i < _productos.size(); i++) {
+        if (NuevoProducto.UPC == _productos[i].UPC) {
+            cout << _productos[i].name << " eliminado exitosamente." << endl;
+            //ELIMINAR ESPACIO COMPLETO DEL VECTOR
+        }
+    }
 }
 void Productos::Ver_Productos(){
+    cout << "Would you prefer to order or to fliter the products?" << endl;
+    cout << "1. Order"
+    << "\n2. Filter" << endl;
+    
 }
 void Productos::Editar_Productos(){
 }
 void Productos::Decision_IVA(){
     int opc_iva = 0;
-    cout << _name << " has IVA? " << endl;
+    cout << NuevoProducto.name << " has IVA? " << endl;
     cout << "(1. YES/2.NO): ";
     cin >> opc_iva;
     switch(opc_iva){
         case 1:
         {
-            _has_iva = true;
+            NuevoProducto.has_iva = true;
             break;
         }
         case 2:
         {
-            _has_iva = false;
+            NuevoProducto.has_iva = false;
             break;
         }
     }
--- Productos.h
+++ Productos.h
@@ -2,12 +2,25 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 using namespace std;
+
+
+struct product{
+    int id;
+    string UPC;
+    string name;
+    int id_presentation;
+    float price;
+    float cost;
+    bool has_iva;
+    int stock;
+};
 
 class Productos
 {
 public:
-	Productos(int id, string UPC, string name, int id_p, float price, float cost, bool has_iva, int stock);
+	Productos();
 	~Productos();
     
     void Menu_Principal();
@@ -17,32 +30,6 @@ public:
     void Eliminar_Productos();
     void Decision_IVA();
 
-    
-    void set_id(int id);
-    void set_UPC(string UPC);
-    void set_name(string name);
-    void set_id_p(int id_p);
-    void set_price(float price);
-    void set_cost(float cost);
-    void set_h_iva(bool has_iva);
-    void set_stock(int stock);
-    
-    int get_id();
-    string get_UPC();
-    string get_name();
-    int get_id_p();
-    float get_price();
-    float get_cost();
-    int get_iva();
-    int get_stock();
-    
 private:
-    int _id;
-    string _UPC;
-    string _name;
-    int _id_presentation;
-    float _price;
-    float _cost;
-    bool _has_iva;
-    int _stock;
+    vector <product> _productos;
 };
